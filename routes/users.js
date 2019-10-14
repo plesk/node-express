b91aa86b50bc5d072235115f9b95fab48f3d4c4a
Mohammad.Khalid95 <muhammad.khalid@bykea.com>
2019-10-14 11:53:38 +0500

    fixed issue

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/userController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-app.post('/register',UserController.register);
-app.get('/allUsers',UserController.getAllUser);
-app.post('/login',UserController.login);
+router.post('/register',UserController.register);
+router.get('/allUsers',UserController.getAllUser);
+router.post('/login',UserController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 const http = require('http');
 
-http.createServer(app).listen(process.env.PORT);
+http.createServer(app).listen(process.env.PORT || 8080);
